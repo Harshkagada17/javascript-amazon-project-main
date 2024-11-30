885f5615ba7acd64e9d84d4f4ce702cdd7cc7577
Harshkagada17 <hkagada522@gmail.com>
2024-11-30 12:02:16 +0530

    Delte Product From Html when clicking Delete

--- script/checkout.js
+++ script/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem) => {
 
     if (matchingProduct) {
         cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -103,9 +103,11 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener("click",()=>{
         const productId = link.dataset.productId;
-        // console.log(productId);
          removefromCart(productId);
-         console.log(cart);
-    });
+
+         const container =  document.querySelector(`.js-cart-item-container-${productId}`);
+         
+         container.remove();
+        });
 });
 // console.log(cartSummaryHTML);
