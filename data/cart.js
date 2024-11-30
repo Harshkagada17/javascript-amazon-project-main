44322fb85c9373c4123568662f15e43b8c792ebf
Harshkagada17 <hkagada522@gmail.com>
2024-11-30 13:53:52 +0530

    Save Cart in LocalStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantitay: 2,
-},{
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantitay:1
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantitay: 2,
+  },{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantitay:1
+  }
+  ];
 }
-];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addTocart(productId){
     let matchingcartItem;
@@ -25,8 +33,8 @@ export function addTocart(productId){
         quantitay:quantitay
       });
     }
+    saveToStorage();
   }
-
   export function removefromCart(productId){
     const newCart = [];
 
@@ -36,4 +44,6 @@ export function addTocart(productId){
       }
     });
     cart = newCart;
+
+    saveToStorage();
   }
